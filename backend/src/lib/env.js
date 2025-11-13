733a457752793523ce1831a83b8b7a3a51fc911c
Rajeev Kumar Sudhansu <rajeevkumarr1221@gmail.com>
2025-11-13 20:20:29 +0530

    deployment config added

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -5,6 +5,7 @@ dotenv.config({ quiet: true });
 export const ENV = {
   PORT: process.env.PORT,
   DB_URL: process.env.DB_URL,
+  NODE_ENV: process.env.NODE_ENV,
   INNGEST_EVET_KEY: process.env.INNGEST_EVET_KEY,
   INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY,
   STREAM_API_KEY: process.env.STREAM_API_KEY,
