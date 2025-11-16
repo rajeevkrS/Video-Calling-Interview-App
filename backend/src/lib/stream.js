eed9200d525f802a1fe88cf3e424025e4b7dd6a1
Rajeev Kumar Sudhansu <rajeevkumarr1221@gmail.com>
2025-11-16 20:59:06 +0530

    minor changes

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -7,7 +7,7 @@ const apiSecret = ENV.STREAM_SECRET_KEY;
 
 // Check if API key and Secret key are available
 if (!apiKey || !apiSecret) {
-  console.error(" Stream API key or Secret key is missing!");
+  console.error("Stream API key or Secret key is missing!");
 }
 
 // Create and export the Stream Chat client instance
