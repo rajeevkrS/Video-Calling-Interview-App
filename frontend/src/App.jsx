ec1e288f9f9dd8c30d6f912ab6cca56a402c15cc
Rajeev Kumar Sudhansu <rajeevkumarr1221@gmail.com>
2025-11-22 19:54:23 +0530

    Landing Page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,31 @@
 import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import ProblemPage from "./pages/ProblemPage";
+import DashboardPage from "./pages/DashboardPage";
 import { useUser } from "@clerk/clerk-react";
 import { Toaster } from "react-hot-toast";
 
 function App() {
-  const { isSignedIn } = useUser();
+  const { isSignedIn, isLoaded } = useUser();
+
+  // this will prevent flickering effect while checking auth state
+  if (!isLoaded) {
+    return null; // or a loading spinner
+  }
+
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />}
+        />
+
+        <Route
+          path="/dashboard"
+          element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />}
+        />
+
         <Route
           path="/problem"
           element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />}
