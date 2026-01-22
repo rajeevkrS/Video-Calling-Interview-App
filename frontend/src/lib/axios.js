f0717a3aad23f62aa28220682ead39d24cb0228a
Rajeev Kumar Sudhansu <rajeevkumarr1221@gmail.com>
2026-01-22 18:42:59 +0530

    deploy on vercel changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.DEV
+    ? import.meta.env.VITE_API_URL // Dev mode
+    : "/api", // Production Mode
   withCredentials: true, // by adding this field browser will send the cookies to server automatically, on every single request
 });
 
